40ec2b805f1fd171a028637c1c52b72e6a01f1f5
aethelstanwindsor <122472192+aethelstanwindsor@users.noreply.github.com>
2023-01-11 16:51:51 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
 streamlit.title('My super awesome diner website thing')
 
-streamlit.text('this is the first piece of text')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
